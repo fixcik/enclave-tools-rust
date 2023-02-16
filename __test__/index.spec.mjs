59407e19970b477a35f03ce1938d4f33b32bc6df
Ivan Chirkin <chirkin.ivan@gmail.com>
2023-02-17 02:15:21 +0300

    fix: tests

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,24 +1,35 @@
 import test from "ava";
 import os from "os";
 import fs from "fs/promises";
+import path from "path";
 
 import { DeduplicateStrategy, MergeStrategy, merge } from "../index.js";
+
+async function getTempFilePath() {
+  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "tmp-"));
+  const tempFileName = "temp-file.txt";
+  return path.join(tempDir, tempFileName);
+}
 
 for (let mergeStrategy in MergeStrategy) {
   for (let deduplicateStrategy in DeduplicateStrategy) {
     test(`merge: ${mergeStrategy} - ${deduplicateStrategy}`, async (t) => {
-      await merge(
-        "./__test__/fixtures/list1-sorted.csv",
-        "./__test__/fixtures/list2-sorted.csv",
-        {
-          mergeStrategy: MergeStrategy[mergeStrategy],
-          deduplicateStrategy: DeduplicateStrategy[deduplicateStrategy],
-          leftKey: "key",
-          rightKey: "key",
-          output: "./result.tsv",
-        }
+      const output = await getTempFilePath();
+      await t.notThrowsAsync(
+        merge(
+          "./__test__/fixtures/list1-sorted.csv",
+          "./__test__/fixtures/list2-sorted.csv",
+          {
+            mergeStrategy: MergeStrategy[mergeStrategy],
+            deduplicateStrategy: DeduplicateStrategy[deduplicateStrategy],
+            leftKey: "key",
+            rightKey: "key",
+            isNumberKey: true,
+            output,
+          }
+        )
       );
-      t.snapshot(await fs.readFile("./result.tsv", { encoding: "ascii" }));
+      t.snapshot(await fs.readFile(output, { encoding: "ascii" }));
     });
   }
 }
